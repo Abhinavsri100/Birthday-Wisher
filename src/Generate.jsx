d0a3a2f148c780b7f01251e6d36999a43a6ca9dc
Abhinav <srivastavabhinav556@gmail.com>
2025-02-16 01:19:59 +0530

    Name Mandatory issue resolved

--- src/Generate.jsx
+++ src/Generate.jsx
@@ -6,60 +6,68 @@ const Generate = () => {
   const [day, setDay] = useState(1);
   const [month, setMonth] = useState(1);
   const [link, setLink] = useState('');
+  const [data, setData] = useState(false);
   const generateLink = () => {
-    setLink(
-      `https://birthday-wisher.netlify.app/birthday/${name}/${day}/${month}`
-    );
+    if (data) {
+      setLink(
+        `https://birthday-wisher.netlify.app/birthday/${name}/${day}/${month}`
+      );
+    }
   };
   return (
-    <div className='page'>
+    <div className="page">
       <h1>Generate Here</h1>
-      <div className='form'>
+      <div className="form">
         <input
-          type='text'
-          placeholder='Enter Name'
+          type="text"
+          placeholder="Enter Name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            if (e.target.value) setData(true);
+            else setData(false);
+            setName(e.target.value);
+          }}
         />
         <input
-          type='number'
-          placeholder='Enter Day'
+          type="number"
+          placeholder="Enter Day"
           value={day}
           onChange={(e) => setDay(e.target.value)}
           max={31}
           min={1}
         />
         <select value={month} onChange={(e) => setMonth(e.target.value)}>
-          <option value=''>Select Month</option>
-          <option selected value='1'>
+          <option value="">Select Month</option>
+          <option selected value="1">
             January
           </option>
-          <option value='2'>February</option>
-          <option value='3'>March</option>
-          <option value='4'>April</option>
-          <option value='5'>May</option>
-          <option value='6'>June</option>
-          <option value='7'>July</option>
-          <option value='8'>August</option>
-          <option value='9'>September</option>
-          <option value='10'>October</option>
-          <option value='11'>November</option>
-          <option value='12'>December</option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+          <option value="4">April</option>
+          <option value="5">May</option>
+          <option value="6">June</option>
+          <option value="7">July</option>
+          <option value="8">August</option>
+          <option value="9">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
         </select>
       </div>
-      <button className='btn' onClick={() => generateLink()}>
+      <button className="btn" onClick={() => generateLink()}>
         Generate Link
       </button>
+      {!data && <p className="empty">Name is Mandatory</p>}
 
-      {link !== '' ? (
+      {link !== "" ? (
         <>
-          <p className='gen-link'>{link}</p>
+          <p className="gen-link">{link}</p>
           <Link to={`birthday/${name}/${day}/${month}`}>
-            <button className='btn'>Visit Link</button>
+            <button className="btn">Visit Link</button>
           </Link>
         </>
       ) : (
-        ''
+        ""
       )}
     </div>
   );
